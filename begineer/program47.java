7a57922bdc8d38119a2e38eb0e25fbaa50623de9
naveendivi <41729252+naveendivi@users.noreply.github.com>
2018-08-16 12:16:34 +0530

    Create program47.java

---
+++ begineer/program47.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+		 	a[i]=sc.nextInt();
+			
+		}
+		int min = a[0]; 
+		int max = a[0]; 
+
+		for (int i = 1; i < a.length; i++)
+		{
+			if (a[i] > max) 
+			{
+				max = a[i];
+			}
+			if (a[i] < min) 
+			{
+				min = a[i];
+			}
+		}
+
+		System.out.println(  max);
+		System.out.println(min);	
+		
+		}	
+	}
+	
+		
