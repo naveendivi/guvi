848524210192541f3051389e109852cc1c72e24b
naveendivi <41729252+naveendivi@users.noreply.github.com>
2018-08-17 11:36:28 +0530

    Create program55.java

---
+++ begineer/program55.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a=sc.nextInt();
+		int m=n*a;
+		if((m%2)==0)
+		{
+			System.out.print("even");
+			
+		}
+		else
+		{
+			System.out.print("odd");
+		}
+		}
+	}
+}
