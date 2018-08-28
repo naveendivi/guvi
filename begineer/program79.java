32a8fa64d8f79b20774fc601b314e83efd475cce
naveendivi <41729252+naveendivi@users.noreply.github.com>
2018-08-28 11:38:06 +0530

    Update program79.java

--- begineer/program79.java
+++ begineer/program79.java
@@ -8,9 +8,9 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
 		int m=sc.nextInt();
-		int c=m*n;
+		int n=sc.nextInt();
+		int c=n*m;
 		if((c%m)==0)
 		{
 			System.out.print("yes");
