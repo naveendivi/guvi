e321849a8ad3df98d3fb66f19987b3d7ac2dc048
naveendivi <41729252+naveendivi@users.noreply.github.com>
2018-08-29 11:32:11 +0530

    Create program86.java

---
+++ begineer/program86.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		String[] ary = s.split("");
+        Set<String> mySet = new HashSet<String>(Arrays.asList(ary));
+
+        if(s.length() == mySet.size()){
+            System.out.print( "Yes");
+        }else{
+            System.out.print( "NO");
+        }
+	}
+}
