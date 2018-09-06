4c4498a718527365b41258e93618a1b2049bf415
naveendivi <41729252+naveendivi@users.noreply.github.com>
2018-09-06 14:39:39 +0530

    Create program95.java

---
+++ begineer/program95.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		long n=sc.nextLong();
+		long c=sc.nextLong();
+		long d=sc.nextLong();
+		long e=(n*c*d)/100;
+		System.out.print(Math.abs(e));
+	}
+}
