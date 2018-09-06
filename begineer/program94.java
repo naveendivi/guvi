430e06225f88fd058930331b90a035698db18900
naveendivi <41729252+naveendivi@users.noreply.github.com>
2018-09-06 15:37:19 +0530

    Create program94.java

---
+++ begineer/program94.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+			
+		}
+		System.out.print(a[k-1]);
+	}
+}program
