cbec0205234b9381248b6362b3521d8aaf1d44c7
naveendivi <41729252+naveendivi@users.noreply.github.com>
2018-09-08 10:25:25 +0530

    Create program74.java

---
+++ begineer/program74.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		float n=sc.nextFloat();
+		System.out.print(Math.round(n));
+	}
+}
