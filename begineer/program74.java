20949cb5bfdf8ee64de12b2ea85a8f89b3ec8d93
naveendivi <41729252+naveendivi@users.noreply.github.com>
2018-09-08 11:35:38 +0530

    Update program74.java

--- begineer/program74.java
+++ begineer/program74.java
@@ -9,6 +9,7 @@ class Ideone
 	{
 		Scanner sc=new Scanner(System.in);
 		float n=sc.nextFloat();
-		System.out.print(Math.round(n));
+		int r=(int)Math.ceil(n)
+		System.out.print(Math.ceil(n));
 	}
 }
